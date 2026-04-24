2dbb9fa500142f65036feb4de699a3a7566ab1cd
Anais Tournier <anais.tournier16@gmail.com>
2026-04-24 16:22:49 +0200

    classe Move

---
+++ include/Move.h
@@ -0,0 +1,22 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+
+class Move
+{
+    private:
+        int m_row;
+        int m_col;
+
+    public:
+        Move();
+        ~Move();
+
+        int getRow() { return m_row; }
+        void setRow(int val) { m_row = val; }
+        int getCol() { return m_col; }
+        void setCol(int val) { m_col = val; }
+
+};
+
+#endif // MOVE_H
