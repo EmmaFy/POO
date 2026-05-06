7051157a6878462a399ce5c3c13ba35d4f5496a4
Emma <90641684+Nausykaa@users.noreply.github.com>
2026-05-06 13:03:33 +0200

    commentaires

--- src/RandomStrategy.cpp
+++ src/RandomStrategy.cpp
@@ -22,12 +22,12 @@ Move RandomStrategy::chooseMove(const UltimateBoard& board) override
             return Move{-1, -1};
         }
 
-        // 2. Initialiser le générateur de nombres aléatoires
+        // Initialiser le générateur de nombres aléatoires
         // static pour ne pas le réinitialiser à chaque appel
         static std::random_device rd;
         static std::mt19937 gen(rd());
 
-        // 3. Définir une distribution sur l'index du vecteur
+        // Définir une distribution sur l'index du vecteur
         std::uniform_int_distribution<> dis(0, legalMoves.size() - 1);
 
         return legalMoves[dis(gen)];
